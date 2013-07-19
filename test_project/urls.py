01ef2e5914f8310c01197f90db557d8ed44c7b1d
Josh Marlow <joshmarlow@gmail.com>
2013-07-19 11:24:06 -0400

    * pep8

--- test_project/urls.py
+++ test_project/urls.py
@@ -11,8 +11,8 @@ urlpatterns = patterns(
     # url(r'^test_project/', include('test_project.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-     url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
