77f99ff75833b7fcd2d161b87b34482cc96fe076
Josh Marlow <joshmarlow@gmail.com>
2013-10-08 10:28:22 -0400

    * Added some of the narrative models to the Django admin

---
+++ narrative/admin.py
@@ -0,0 +1,11 @@
+"""
+Admin for platform integration.
+"""
+from django.contrib.admin import site
+
+from . import models
+
+
+site.register(models.Datum)
+site.register(models.AssertionMeta)
+site.register(models.EventMeta)
