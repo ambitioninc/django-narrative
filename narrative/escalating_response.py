7f8de9f936a7675f3a80736661dddfa40d5fc092
Josh Marlow <joshmarlow@gmail.com>
2013-10-11 14:06:37 -0400

    * Added a new 'escalating response' convenience function for * constructing multi-step plans.

---
+++ narrative/escalating_response.py
@@ -0,0 +1,14 @@
+def construct_escalating_response(issue, response_map):
+    """
+    Sometimes we need to have a response that can escalate
+    if not addressed in a certain amount of time.  We support
+    this in this function by constructing a response based on a
+    'response_map' that maps a given response to a specific
+    escalation_number (ie, how many previous responses there
+    has been to this issue).
+    """
+    non_pass_steps = issue.get_non_pass_steps()
+
+    print 'constructing response', non_pass_steps
+
+    return response_map.get(len(non_pass_steps), lambda x: None)(non_pass_steps)
