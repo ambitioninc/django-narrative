034f0ab8204d731da0c4815bfb3cc531ca9d190d
Josh Marlow <joshmarlow@gmail.com>
2013-10-21 15:36:39 +0000

    * Bumped the version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='narrative',
-    version='0.5.3.5',
+    version='0.5.3.6',
     packages=[
         'narrative',
         'narrative.batteries',
