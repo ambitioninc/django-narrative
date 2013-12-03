02721e943b68a530b3086392c8108b412d1444ea
Wes Okes <wes.okes@gmail.com>
2013-12-03 10:21:13 -0500

    * bumped to 0.5.4.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='narrative',
-    version='0.5.4.6',
+    version='0.5.4.7',
     packages=[
         'narrative',
         'narrative.batteries',
